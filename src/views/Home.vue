dafd1ca415e68337ea25d2691668b32b16173693
Jason Cameron <jasoncameron246@gmail.com>
2024-03-07 19:25:53 -0500

    styles for buttons added, and ticket component scaffoled.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
+  <div class="wrapper">
     <Navbar />
+    <Ticket />
     <Footer />
   </div>
 </template>
@@ -8,6 +9,7 @@
 <script setup>
 import Navbar from "../components/Navbar.vue";
 import Footer from "../components/Footer.vue";
+import Ticket from "../components/Ticket.vue";
 </script>
 
 <style lang="scss" scoped></style>
