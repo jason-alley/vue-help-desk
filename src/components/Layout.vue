e9646e93abdef5de75a25c28822cddf190bfa70d
Jason Cameron <jasoncameron246@gmail.com>
2024-03-08 12:14:00 -0500

    layout component scaffoled.

---
+++ src/components/Layout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="container">
+      <slot></slot>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import Navbar from "./Navbar.vue";
+import Footer from "./Footer.vue";
+</script>
+
+<style scoped>
+.container {
+  margin: 2rem auto;
+  max-width: 800px;
+}
+</style>
