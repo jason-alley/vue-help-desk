7107d3e7be288b746fbb6188991704db3b4a8367
Jason Cameron <jasoncameron@Jasons-MacBook-Pro-2.local>
2024-03-25 13:13:01 -0400

    added dialog element to show individual ticket data.

---
+++ src/components/Modal.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+    showModal: {
+        type: String,
+        required: true
+    }
+})
+
+
+const dialog = document.querySelector("dialog");
+
+// "Show the dialog" based on the "showModal" prop
+if( props.showModal === "true" ) {
+    dialog.show();
+}
+
+
+// "Close" button closes the dialog
+const closeModal = () => {
+    dialog.close();
+}
+
+</script>
+
+<template>
+    <dialog closed>
+        <h3>Ticket Name</h3>
+        <form method="dialog">
+            <p>Hello from dialog box</p>
+            <button @click="closeModal">Close</button>
+        </form>
+    </dialog>
+</template>
--- src/components/TicketsCategories.vue
+++ src/components/TicketsCategories.vue
@@ -1,14 +1,19 @@
 <script setup>
-import { ref, computed } from 'vue';
-
+import { ref, computed, onMounted } from 'vue';
 import Ticket from './Ticket.vue';
+
+// const dialog = document.querySelector("dialog");
+const dialog = ref(null);
+let showModal = ref("false");
 
 const props = defineProps({
     tickets: {
         required: true,
         type: Array,
-    },
+    }
 });
+
+
 
 /**
  * This filters the tickets json data by ticket status, and returns an array of tickets that match the status.
@@ -25,43 +30,60 @@ const inProgressTickets = computed(() => filterTicketsByStatus(props.tickets, 'I
 const completedTickets = computed(() => filterTicketsByStatus(props.tickets, 'Completed'));
 
 
+/**
+ * Opens the dialog element.
+ * @param {Number} id
+ */
+function openModal(id) {
+    showModal = "true";
+
+    if (showModal === "true") {
+        dialog.value.showModal();
+    }
+
+    // get the ticket with the id.
+    const ticket = props.tickets.find(ticket => ticket.id === id);
+    console.log(ticket, 'the selected ticket');
+
+}
+
+/**
+ * Closes the modal by calling the `close` method on the `dialog` object.
+ *
+ * @param {type} - No parameters.
+ * @return {type} - No return value.
+ */
+const closeModal = () => {
+    dialog.value.close();
+}
+
 
 </script>
 
 <template>
+    <dialog ref="dialog" closed>
+        <h3>Ticket Name</h3>
+        <form method="dialog">
+            <p>Hello from dialog box</p>
+            <button @click="closeModal">Close</button>
+        </form>
+    </dialog>
     <div class="pure-g">
         <div class="pure-u-1-3 tickets-col">
             <p>Todo</p>
-            <Ticket v-for="ticket in newTickets" 
-                :key="ticket.id"
-                :id="+ticket.id"
-                :title="ticket.title"
-                :description="ticket.description"
-                :status="ticket.status"
-                :priority="ticket.priority"
-            />
+            <Ticket v-for="ticket in newTickets" :key="ticket.id" :id="+ticket.id" :title="ticket.title"
+                :description="ticket.description" :status="ticket.status" :priority="ticket.priority"
+                @click="openModal(ticket.id)" />
         </div>
         <div class="pure-u-1-3 tickets-col">
             <p>In Progress</p>
-            <Ticket v-for="ticket in inProgressTickets" 
-                :key="ticket.id"
-                :id="+ticket.id"
-                :title="ticket.title"
-                :description="ticket.description"
-                :status="ticket.status"
-                :priority="ticket.priority"
-            />
+            <Ticket v-for="ticket in inProgressTickets" :key="ticket.id" :id="+ticket.id" :title="ticket.title"
+                :description="ticket.description" :status="ticket.status" :priority="ticket.priority" />
         </div>
         <div class="pure-u-1-3 tickets-col">
             <p>Completed</p>
-            <Ticket v-for="ticket in completedTickets" 
-                :key="ticket.id"
-                :id="+ticket.id"
-                :title="ticket.title"
-                :description="ticket.description"
-                :status="ticket.status"
-                :priority="ticket.priority"
-            />
+            <Ticket v-for="ticket in completedTickets" :key="ticket.id" :id="+ticket.id" :title="ticket.title"
+                :description="ticket.description" :status="ticket.status" :priority="ticket.priority" />
         </div>
     </div>
 </template>
